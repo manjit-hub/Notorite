25a57a71961ed550c135693c43d51439bd5d4633
anuj-pal27 <anujpal27669@gmail.com>
2024-10-02 23:01:44 +0530

    UI updated

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,12 +5,13 @@ import { MdOutlineFileUpload } from "react-icons/md";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { useDispatch, useSelector } from "react-redux";
 import { removeUserData, setUserData } from "../Redux/slices/user-slice";
-import { Switch, useColorMode } from "theme-ui";
+import { useColorMode } from "theme-ui";
 import { useState } from "react";
+import { MdDarkMode, MdLightMode } from "react-icons/md";
 const Navbar = () => {
-
   const [colorMode, setColorMode] = useColorMode();
   const [state, setState] = useState("dark");
+  const [buttonState, setButtonState] = useState(true);
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -21,24 +22,51 @@ const Navbar = () => {
   const handleLogout = () => {
     dispatch(removeUserData());
     navigate("/");
-  }
+  };
 
   return (
-    <header className="flex h-[80px] items-center justify-center shadow-md">
-      <div className="mx-5 flex w-full max-w-[1550px] items-center justify-between">
+    <header
+      className={
+        colorMode === "light"
+          ? "flex h-[80px] items-center justify-center bg-[#0056C1] shadow-md"
+          : "flex h-[80px] items-center justify-center bg-stone-900 shadow-md"
+      }
+    >
+      <div className="mx-3 flex w-full max-w-[1550px] items-center justify-between">
         {/* image section */}
-        <div className="flex h-[120px] w-[120px] items-center justify-center overflow-hidden">
+        <div className="ml-[-90px] flex h-[120px] w-[120px] items-center justify-center overflow-hidden">
           <Link to="/">
-          <img src="/logo.png" alt="Logo" />
+            <img src="/logo.png" alt="Logo" className="" />
           </Link>
         </div>
         {/* nav links  */}
-        <GiHamburgerMenu className="text-xl md:hidden text-white" />
-        <div className="hidden md:flex md:items-center md:justify-center md:gap-4">
-          <Link to="/" className="">
+        <GiHamburgerMenu className="text-xl text-white md:hidden" />
+        <div className="mr-[-90px] hidden md:flex md:items-center md:justify-center md:gap-4">
+          <div className="text-xl">
+            {buttonState ? (
+              <MdDarkMode
+                className="text-white"
+                onClick={() => {
+                  setColorMode(colorMode === "light" ? "dark" : "light");
+                  setState(colorMode === "light" ? "dark" : "light");
+                  setButtonState(false);
+                }}
+              />
+            ) : (
+              <MdLightMode
+                className="text-white"
+                onClick={() => {
+                  setColorMode(colorMode === "dark" ? "light" : "dark");
+                  setState(colorMode === "dark" ? "light" : "dark");
+                  setButtonState(true);
+                }}
+              />
+            )}
+          </div>
+          <Link to="/" className="text-white">
             Home
           </Link>
-          <Link to="/about" className="">
+          <Link to="/about" className="text-white">
             About
           </Link>
 
@@ -46,44 +74,58 @@ const Navbar = () => {
           {isAuthenticated ? (
             <>
               <Link to="/search">
-                <FaSearch className="text-xl " />
+                <FaSearch className="text-xl" />
               </Link>
               <Link to="/upload">
-                <MdOutlineFileUpload className="text-[24px] " />
+                <MdOutlineFileUpload className="text-[24px]" />
               </Link>
               <Link to="/profile">
-              <button className="rounded-xl bg-sky-500 px-5 py-2 font-semibold text-white hover:bg-sky-600">
-                Profile
-              </button>
+                <button
+                  className={
+                    colorMode === "light"
+                      ? "rounded-xl bg-sky-500 px-5 py-2 font-semibold text-white hover:bg-sky-600"
+                      : "rounded-xl bg-stone-700 px-5 py-2 font-semibold text-white hover:bg-sky-600"
+                  }
+                >
+                  Profile
+                </button>
               </Link>
-              <button className="rounded-xl bg-sky-500 px-5 py-2 font-semibold text-white hover:bg-sky-600" onClick={handleLogout}>
+              <button
+                className={
+                  colorMode === "light"
+                    ? "rounded-xl bg-sky-500 px-5 py-2 font-semibold text-white hover:bg-sky-600"
+                    : "rounded-xl bg-stone-700 px-5 py-2 font-semibold text-white hover:bg-sky-600"
+                }
+              >
                 Logout
               </button>
             </>
           ) : (
             <>
               <Link to="/login">
-                <button className="rounded-xl bg-sky-500 px-5 py-2 font-semibold text-white hover:bg-sky-600">
+                <button
+                  className={
+                    colorMode === "light"
+                      ? "rounded-xl bg-sky-500 px-5 py-2 font-semibold text-white hover:bg-sky-600"
+                      : "rounded-xl bg-stone-700 px-5 py-2 font-semibold text-white hover:bg-sky-600"
+                  }
+                >
                   Login
                 </button>
               </Link>
               <Link to="/signup">
-                <button className="rounded-xl bg-sky-500 px-5 py-2 font-semibold text-white hover:bg-sky-600">
+                <button
+                  className={
+                    colorMode === "light"
+                      ? "rounded-xl bg-sky-500 px-5 py-2 font-semibold text-white hover:bg-sky-600"
+                      : "rounded-xl bg-stone-700 px-5 py-2 font-semibold text-white hover:bg-sky-600"
+                  }
+                >
                   Signup
                 </button>
               </Link>
             </>
-
           )}
-                    <div>
-  <div>{state === "light" ? <div> Dark</div> : <div>Light</div>}</div>
-  <Switch
-    onClick={() => {
-      setColorMode(colorMode === "dark" ? "light" : "dark");
-      setState(colorMode === "dark" ? "light" : "dark");
-    }}
-  />
-</div>
         </div>
       </div>
     </header>
--- client/src/theme.js
+++ client/src/theme.js
@@ -1,19 +1,20 @@
 const theme = {
-    initialColorMode: "dark",
-    colors: {
-      text: "black",
-      background: "white",
-      primary: "#1408e6",
-      inverseText: "blue",
-      modes: {
-        dark: {
-          text: "white",
-          background: "#282c34",
-          primary: "#8fceff",
-          inverseText: "green",
-        },
+  initialColorMode: "light",
+  colors: {
+    text: "black",
+    background: "white",
+    primary: "#1408e6",
+    inverseText: "blue",
+    modes: {
+      dark: {
+        text: "white",
+        // background: "#282c34",
+        background: "rgb(41 37 36)",
+        primary: "#8fceff",
+        inverseText: "green",
       },
     },
-  };
-  
-  export default theme;
+  },
+};
+
+export default theme;
