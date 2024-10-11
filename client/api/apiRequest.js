da6f881a371206e5f4c5252733fdfb552e1d746d
Shubham Srivastava <shubhamsri2257@gmail.com>
2024-10-11 17:35:51 +0530

    Functions added

--- client/api/apiRequest.js
+++ client/api/apiRequest.js
@@ -1,51 +1,62 @@
 import axios from "axios";
 import "dotenv";
 
-const signup = async (e) => {
+const signup = async (formData) => {
   try {
-    e.preventDefault();
-
-    if (!isEmailVerified) {
-      toast.error("Please verify your email before registering.");
-      return;
-    }
-    if (!userEmail.endsWith("@gmail.com")) {
-      toast.error("Invalid email domain. Must be @gmail.com");
-      return;
-    }
-
-    if (userPassword.length < 8) {
-      toast.error("Password must be at least 8 characters long");
-      return;
-    }
-
-    const formData = new FormData();
-    formData.append("firstName", firstName);
-    formData.append("lastName", lastName);
-    formData.append("userBio", userBio);
-    formData.append("userEmail", userEmail);
-    formData.append("userName", userName);
-    formData.append("userPassword", userPassword);
-    formData.append("profileImage", profileImage);
-
+    const config = {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    };
     const result = await axios.post(
       `${process.env.VITE_BACKEND_URL}/auth/signup`,
       formData,
-      {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      },
+      config,
     );
-    toast.success("User Entry Saved in Database");
-    console.log("Data: ", result);
-    setTimeout(() => {
-      navigate("/login");
-    }, 1000);
+    return result;
   } catch (error) {
-    toast.error("Failed to Register User");
-    console.log("Failed to Register User: ", error);
+    console.error("User not registered ", error);
+    return error;
   }
 };
 
-export default signup;
+const login = async (user) => {
+  try {
+    const result = await axios.post(
+      `${process.env.VITE_BACKEND_URL}/auth/login`,
+      user,
+    );
+    if (result.data.status === "Error") {
+      console.error("Error while Log in !!");
+    } else {
+      console.log("User Logged in Successfully: ", result);
+      setTimeout(() => {
+        navigate("/");
+      }, 1000);
+    }
+    return result;
+  } catch (error) {
+    console.error("Cannot log in the user: ", error);
+    return result;
+  }
+};
+
+const forgetPassword = async (forgotEmail) => {
+  try {
+    const result = await axios.post(
+      `${process.env.VITE_BACKEND_URL}/auth/forgot-password`,
+      {
+        email: forgotEmail,
+      },
+    );
+    if (result.data.status === "Ok") {
+      console.log("Password reset email sent!");
+    } else {
+      console.error("Error sending reset email.");
+    }
+  } catch (error) {
+    console.error("Failed to send reset email.", error);
+  }
+};
+
+export default { signup, login, forgetPassword };
