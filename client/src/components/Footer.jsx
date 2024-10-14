82de2c8b092dae80a8eabc041889940572444f79
Abhii <abhishekhsharma2002@gmail.com>
2024-10-14 10:52:07 +0530

    footer alignment on small screen fixed

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <footer className="bg-gray-200 dark:bg-stone-900 flex items-center justify-center p-16">
-      <div className="flex h-full w-full flex-col gap-10 px-20 lg:flex-row lg:justify-between">
+      <div className="flex h-full w-full flex-col gap-10 px-0 sm:px-20 lg:flex-row lg:justify-between">
         <div className="lg:w-[450px]">
           <h2 className="relative mb-3 text-2xl font-black text-gray-900 dark:text-white before:absolute before:top-[30px] before:h-[3px] before:w-[200px] before:bg-yellow-400">
             About Us
