862f90650fb8448cb22973d465b2d737ac6c3628
Satvik1769 <manchandasatvik@gmail.com>
2024-10-18 18:37:02 +0530

    fix: login refreshing bug

--- client/src/Redux/slices/user-slice.js
+++ client/src/Redux/slices/user-slice.js
@@ -1,19 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+  userData: JSON.parse(localStorage.getItem("userData")) || null,
+  isAuthenticated: !!localStorage.getItem("userData"),
+};
+
 const userSlice = createSlice({
   name: "user",
-  initialState: {
-    userData: null,
-    isAuthenticated: false,
-  },
+  initialState,
   reducers: {
     setUserData: (state, action) => {
       state.userData = action.payload;
       state.isAuthenticated = true;
+      // Store user data in localStorage
+      localStorage.setItem("userData", JSON.stringify(action.payload));
     },
-    removeUserData: (state, action) => {
+    removeUserData: (state) => {
       state.userData = null;
       state.isAuthenticated = false;
+      // Remove user data from localStorage
+      localStorage.removeItem("userData");
     },
   },
 });
